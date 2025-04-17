d0e6a19ad698ff14234aab2ce5cf2e26d5b3ebd8
Kunle123 <kunle2000@gmail.com>
2025-04-17 16:31:57 +0100

    Revert "Add CORS configuration to API Gateway with support for Vercel domain"
    This reverts commit 1a1b1ed420ccdf75729a911847fa554f2e6815a1.

--- backend/api_gateway/src/index.js
+++
@@ -1,9 +0,0 @@
-const corsOptions = {
-  origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: true,
-  maxAge: 86400 // 24 hours
-};
-
-app.use(cors(corsOptions)); 
